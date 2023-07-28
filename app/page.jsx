70c944310b7a5418883ed0107669d4ad342f5414
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-07-28 10:21:41 +0100

    Featured Review in the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,30 @@
-// import { readFile } from 'node:fs/promises';
-// import { marked } from 'marked';
+
 import Heading from '../components/Heading';
+import Link from 'next/link';
+import { getFeaturedReview } from '../lib/reviews';
 
-export default async function StardewValleyPage() {
-  // const text = await readFile('/content/reviews/stardew-valley.md','utf8');
-
-  // const html = marked(text, { headerIds: false, mangle: false });
-
+export default async function HomePage() {
+  const review = await getFeaturedReview();
+  console.log('[HomePage] rendering');
   return (
     <>
-      <Heading>Stardew Valley</Heading>
-      <img src="/images/stardew-valley.jpg" alt=""
-        width="640" height="360" className="mb-2 rounded"
-      />
-    
-      {/* <article dangerouslySetInnerHTML={{ __html: html }} /> */}
-    </>
+      <Heading>Indie Gamer</Heading>
+      <p className="pb-3">
+        Only the best indie games, reviewed for you.
+      </p>
+      <div className="bg-white border rounded shadow w-80
+                      hover:shadow-xl sm:w-full">
+        <Link href={`/reviews/${review.slug}`}
+          className="flex flex-col sm:flex-row">
+          <img src={review.image} alt=""
+            width="320" height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
+            {review.title}
+          </h2>
+        </Link>
+      </div>
+      </>
   );
 }
--- content/reviews/hollow-knight.md
+++ content/reviews/hollow-knight.md
@@ -1,6 +1,6 @@
 ---
 title: "Hollow Knight"
-date: "2023-05-05"
+date: "2021-05-05"
 image: "/images/hollow-knight.jpg"
 ---
 
