4747bc94f17f0789bcda1249b2a7b01706f1a996
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-07-28 09:52:09 +0100

    statuc generation of dynamic routes

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,38 +1,34 @@
 import Heading from '../../components/Heading'
 import Link from "next/link";
-import { getReviews } from '../../lib/reviews';
+import { getReviews, getSlugs } from '../../lib/reviews';
 
+export const generateStaticParams = async () => {
+  const slugs = await getSlugs();
+  return slugs.map(slug=>{ slug });
 
+}
 
 export default async function ReviewsPage() {
   const reviews = await getReviews();
-  console.log('[reviewsPage] reviews : ',reviews)
+  console.log('rendreing');
   return (
     <>   
-      <Heading>Reviews</Heading>
-      <header>
-        <nav>
-          <ul className="flex flex-col gap-3">
-            {reviews.map((review) => {
-                <li key={review.slug} className="bg-white border w-80 rounded shadow hover:shadow-xl">
-                <Link href={`/reviews/${review.slug}`}>
-                  <img
-                    src={review.image}
-                    alt=""
-                    width="320"
-                    height="180"
-                    className="rounded-t"
-                  />
-                  <h2 className="font-semibold font-orbitron text-center py-1 ">
-                    {review.title}
-                  </h2>
-                </Link>
-              </li>
-            })}         
-
-          </ul>
-        </nav>
-      </header>
+      <Heading>{reviews.title}</Heading>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li key={review.slug}
+            className="bg-white border rounded shadow w-80 hover:shadow-xl">
+            <Link href={`/reviews/${review.slug}`}>
+              <img src={review.image} alt=""
+                width="320" height="180" className="rounded-t"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
