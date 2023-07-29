418d7c41efcc6bb39919d4d3459bd5ac9a494b73
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-07-29 06:51:07 +0100

    Strapi API Parameters

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,9 +1,18 @@
+import { writeFileSync } from "node:fs";
+import qs from "qs";
 
-import { writeFileSync } from 'node:fs';
-
-const url ='http://localhost:1337/api/reviews'+'?populate=*';
+const url = "http://localhost:1337/api/reviews"
+ + "?" +
+  qs.stringify({
+    fields:['slug','title','subtitle','publishedAt'],
+    populate: {image:{fields:['url']}},
+    //only first 6 elements
+    pagination:{pageSize:6},
+    sort:['publishedAt:desc']
+  },{encodeValuesOnly: true});
+  
 const response = await fetch(url);
 const body = await response.json();
-const formated = JSON.stringify(body,null,2); 
-const file = 'scripts/strapi-response.json';
-writeFileSync(file,formated,'utf-8');
+const formated = JSON.stringify(body, null, 2);
+const file = "scripts/strapi-response.json";
+writeFileSync(file, formated, "utf-8");
