82291d42e68af67f21f6656dcda090dd77230eb4
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-07-30 07:18:13 +0100

    On-Demand Revalidation

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,7 +1,12 @@
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server"
+import { CACH_TAG_REVIEWS } from "../../../lib/reviews";
 
 export  const POST = async(request) => {
   const pyload = await request.json();
   console.log(pyload);
+  if(pyload.model === 'review'){
+    revalidateTag(CACH_TAG_REVIEWS);
+  }
   return new Response(null,{status:204});
 }
